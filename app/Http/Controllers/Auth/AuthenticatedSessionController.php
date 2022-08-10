a8d56f6a8c317624ea2c6e78f5f8016eaebe8130
Jonas Santos <jonas.santos9708@gmail.com>
2022-08-10 12:16:45 -0300

    ajuste do logut

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -49,6 +49,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/login');
+        return redirect()->to('https://projetoecultura.com/inicio/');
     }
 }
